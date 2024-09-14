ac300f0fd226510208051e61b86b561129d242fb
Rajugiri <rajugirigoswami09@gmail.com>
2024-09-14 11:34:04 +0530

    code commit

--- svc.birdcage.model/Implementation/Dapper/DapperServices.cs
+++ svc.birdcage.model/Implementation/Dapper/DapperServices.cs
@@ -1,37 +1,152 @@
-﻿namespace svc.birdcage.model.Implementation.Dapper;
+﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace svc.birdcage.model.Implementation.Dapper;
 
 public class DapperServices : IDapperService
 {
     private IDbConnection _connection;
+    private int? CommandTimeout = 0;
+    private IDbTransaction? Transaction = null;
 
     public DapperServices(string connectionString)
     {
         this._connection = new SqlConnection(connectionString);
     }
-    public Task<IEnumerable<T>> GetTableAsync<T>(string query, object parameters)
+    #region Add or Update single or multiple records.
+
+    public int AddOrUpdate<T>(string query, T entity, CommandType type = CommandType.StoredProcedure)
     {
-        return _connection.QueryAsync<T>(query, param: parameters, commandType: CommandType.StoredProcedure);
+        if (Connection == null)
+            throw new ArgumentNullException(nameof(entity), $"The parameter {nameof(entity)} can't be null");
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(entity, nameof(entity));
+        int result = Connection.Execute(query, entity, Transaction, CommandTimeout, type);
+        return result;
     }
 
-    public Task<int> AddOrUpdateAsync<T>(string query, object parameters)
+    public int AddOrUpdate<T>(string query, IEnumerable<T> entities, CommandType type = CommandType.StoredProcedure)
     {
-        return _connection.ExecuteAsync(query, param: parameters, commandType: CommandType.StoredProcedure);
+        if (Connection == null)
+            throw new ArgumentNullException(nameof(entities), $"The parameter {nameof(entities)} can't be null");
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateEnumerable(entities, nameof(entities));
+        int result = Connection.Execute(query, entities, Transaction, CommandTimeout, type);
+        return result;
     }
 
-    public T Find<T>(string query, object parameters)
+    public Task<int> AddOrUpdateAsync<T>(string query, T entity, CommandType type = CommandType.StoredProcedure)
     {
-        return _connection.QueryFirstOrDefault<T>(query, param: parameters, commandType: CommandType.StoredProcedure) !;
+        if (Connection == null)
+            throw new ArgumentNullException(nameof(entity), $"The parameter {nameof(entity)} can't be null");
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(entity, nameof(entity));
+        var result = Connection.ExecuteAsync(query, entity, Transaction, CommandTimeout, type);
+        return result;
     }
 
-    public Task<T> FindAsync<T>(string query, object parameters)
+    public Task<int> AddOrUpdateAsync<T>(string query, IEnumerable<T> entities, CommandType type = CommandType.StoredProcedure)
+    {
+        if (Connection == null)
+            throw new ArgumentNullException(nameof(entities), $"The parameter {nameof(entities)} can't be null");
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateEnumerable(entities, nameof(entities));
+        var result = Connection.ExecuteAsync(query, entities, Transaction, CommandTimeout, type);
+        return result;
+    }
+
+    #endregion Add or Update single or multiple records.
+
+    #region Get record by Id.
+
+    public T Find<T>(string query, object primarykeyFields, CommandType type = CommandType.StoredProcedure)
+    {
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(primarykeyFields, nameof(primarykeyFields));
+        return Connection.QueryFirstOrDefault<T>(query, primarykeyFields, Transaction, CommandTimeout, type);
+    }
+
+    public Task<T> FindAsync<T>(string query, object primarykeyFields, CommandType type = CommandType.StoredProcedure)
     {
         return Task.Run(() =>
         {
-            return Find<T>(query, parameters);
+            return Find<T>(query, primarykeyFields, type);
         });
     }
-    public Task<SqlMapper.GridReader> GetMultipleAsync(string query, object parameters)
+
+    #endregion Get record by Id.
+
+    #region Get tables
+
+    public List<T> GetTable<T>(string query, object parameters, CommandType type = CommandType.StoredProcedure)
     {
-        return _connection.QueryMultipleAsync(query, parameters, commandType: CommandType.StoredProcedure);
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(parameters, nameof(parameters));
+        return Connection.Query<T>(query, parameters, null, true, CommandTimeout, type).ToList();
     }
+
+    public Task<IEnumerable<T>> GetTableAsync<T>(string query, object parameters, CommandType type = CommandType.StoredProcedure)
+    {
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(parameters, nameof(parameters));
+        return Connection.QueryAsync<T>(query, parameters, Transaction, CommandTimeout, type);
+    }
+
+    #endregion Get tables
+
+    #region Get more than one tables
+
+    public SqlMapper.GridReader GetTables(string query, object parameters, CommandType type = CommandType.StoredProcedure)
+    {
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(parameters, nameof(parameters));
+        return Connection.QueryMultiple(query, parameters, Transaction, CommandTimeout, type);
+    }
+
+    public Task<SqlMapper.GridReader> GetTablesAsync(string query, object parameters, CommandType type = CommandType.StoredProcedure)
+    {
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(parameters, nameof(parameters));
+        return Connection.QueryMultipleAsync(query, parameters, Transaction, CommandTimeout, type);
+    }
+
+    #endregion Get more than one tables
+
+    #region Add or Update single or multiple records & get result.
+
+    public T AddOrUpdateAndGet<T>(string query, T entity, CommandType type = CommandType.StoredProcedure)
+    {
+        if (Connection == null) throw new ArgumentNullException(nameof(entity), $"The parameter {nameof(entity)} can't be null");
+
+        ParameterValidator.ValidateString(query, nameof(query));
+        ParameterValidator.ValidateObject(entity, nameof(entity));
+
+        var result = Connection.QueryFirstOrDefault<T>(query, entity, Transaction, CommandTimeout, type);
+
+        return result;
+    }
+
+    #endregion Add or Update single or multiple records & get result.
+
+    #region House Keeping
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            if (Connection.State != ConnectionState.Closed)
+                Connection.Close();
+
+            Connection.Dispose();
+            Transaction = null;
+            CommandTimeout = null;
+        }
+    }
+
+    #endregion House Keeping
 }
--- svc.birdcage.model/Interfaces/Dapper/IDapperService.cs
+++ svc.birdcage.model/Interfaces/Dapper/IDapperService.cs
@@ -2,8 +2,25 @@
 
 public interface IDapperService
 {
-    Task<IEnumerable<T>> GetTableAsync<T>(string query, object parameters);
-    Task<int> AddOrUpdateAsync<T>(string query, object parameters);
-    Task<T> FindAsync<T>(string query, object parameters);
-    public Task<SqlMapper.GridReader> GetMultipleAsync(string query, object parameters);
+    List<T> GetTable<T>(string query, object parameters, CommandType type = CommandType.StoredProcedure);
+
+    Task<IEnumerable<T>> GetTableAsync<T>(string query, object parameters, CommandType type = CommandType.StoredProcedure);
+
+    SqlMapper.GridReader GetTables(string query, object parameters, CommandType type = CommandType.StoredProcedure);
+
+    Task<SqlMapper.GridReader> GetTablesAsync(string query, object parameters, CommandType type = CommandType.StoredProcedure);
+
+    T Find<T>(string query, object primarykeyFields, CommandType type = CommandType.StoredProcedure);
+
+    Task<T> FindAsync<T>(string query, object primarykeyFields, CommandType type = CommandType.StoredProcedure);
+
+    int AddOrUpdate<T>(string query, T entity, CommandType type = CommandType.StoredProcedure);
+
+    Task<int> AddOrUpdateAsync<T>(string query, T entity, CommandType type = CommandType.StoredProcedure);
+
+    int AddOrUpdate<T>(string query, IEnumerable<T> entities, CommandType type = CommandType.StoredProcedure);
+
+    Task<int> AddOrUpdateAsync<T>(string query, IEnumerable<T> entities, CommandType type = CommandType.StoredProcedure);
+
+    T AddOrUpdateAndGet<T>(string query, T entity, CommandType type = CommandType.StoredProcedure);
 }
